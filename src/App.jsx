fd045257cfe5d5592efd763c27485c336722f695
shivdutt-B <shivdutt059@gmail.com>
2025-03-21 23:58:47 +0530

    add env var

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>HHH {import.meta.env.VITE_APP_API_URL}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
